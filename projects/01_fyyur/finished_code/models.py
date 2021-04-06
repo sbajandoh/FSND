923fc60985dda0e074779398b808cd417031f56e
sbajandoh <sbajandooh@gmail.com>
2021-04-06 16:15:10 +0300

    Create models.py

---
+++ projects/01_fyyur/finished_code/models.py
@@ -0,0 +1,64 @@
+#----------------------------------------------------------------------------#
+# Models.
+#----------------------------------------------------------------------------#
+
+class Venue(db.Model):
+    __tablename__ = 'Venue'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    city = db.Column(db.String(120))
+    state = db.Column(db.String(120))
+    address = db.Column(db.String(120))
+    phone = db.Column(db.String(120))
+    image_link = db.Column(db.String(500))
+    facebook_link = db.Column(db.String(120))
+
+    # TODO: implement any missing fields, as a database migration using Flask-Migrate
+    genres = db.Column(db.String)
+    seeking_talent = db.Column(db.Boolean)
+    upcoming_counter = db.Column(db.Integer, default=0)
+    seekingDescription = db.Column(db.String(500))
+    website = db.Column(db.String(120))
+    def add(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.update(self)
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+
+
+
+class Artist(db.Model):
+    __tablename__ = 'Artist'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    city = db.Column(db.String(120))
+    state = db.Column(db.String(120))
+    phone = db.Column(db.String(120))
+    genres = db.Column(db.String(120))
+    image_link = db.Column(db.String(500))
+    facebook_link = db.Column(db.String(120))
+    # TODO: implement any missing fields, as a database migration using Flask-Migrate
+    address = db.Column(db.String(120))
+    website = db.Column(db.String(120))
+    upcoming_counter = db.Column(db.Integer, default=0)
+    seekingVenue = db.Column(db.Boolean)
+    seekingDescription = db.Column(db.String(500))
+    def add(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.update(self)
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
