63baff0dad1f403d4c3d76ca3475d7e3ebb9a2ba
sbajandoh <sbajandooh@gmail.com>
2021-04-06 17:47:08 +0300

    Update app.py

--- projects/01_fyyur/finished_code/app.py
+++ projects/01_fyyur/finished_code/app.py
@@ -93,6 +93,13 @@ class Artist(db.Model):
         db.session.delete(self)
         db.session.commit()
 
+class Show(db.Model):
+    __tablename__ = 'Show'
+
+    id = db.Column(db.Integer, primary_key=True)
+    artist_id = db.Column(db.Integer, db.ForeignKey('Artist.id'), nullable=False)
+    venue_id = db.Column(db.Integer, db.ForeignKey('Venue.id'), nullable=False)
+    start_time = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
 
 # TODO Implement Show and Artist models, and complete all model relationships and properties, as a database migration.
 
