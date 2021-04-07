f5fad0e7f29b3896c6695c0eb4df8d2ba396a4dd
sbajandoh <sbajandooh@gmail.com>
2021-04-07 07:59:43 +0300

    Update models.py

--- projects/01_fyyur/finished_code/models.py
+++ projects/01_fyyur/finished_code/models.py
@@ -11,21 +11,25 @@ def db_setup(app):
 class Venue(db.Model):
     __tablename__ = 'Venue'
 
+    __tablename__ = 'Venue'
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    city = db.Column(db.String(120))
-    state = db.Column(db.String(120))
-    address = db.Column(db.String(120))
-    phone = db.Column(db.String(120))
+    name = db.Column(db.String, nullable=False)
+    city = db.Column(db.String(120), nullable=False)
+    state = db.Column(db.String(120), nullable=False)
+    address = db.Column(db.String(120), nullable=False)
+    phone = db.Column(db.String(120), nullable=False)
     image_link = db.Column(db.String(500))
     facebook_link = db.Column(db.String(120))
 
     # TODO: implement any missing fields, as a database migration using Flask-Migrate
-    genres = db.Column(db.String)
-    seeking_talent = db.Column(db.Boolean)
-    upcoming_counter = db.Column(db.Integer, default=0)
-    seekingDescription = db.Column(db.String(500))
-    website = db.Column(db.String(120))
+    genres = db.Column(db.String(120),nullable=False)
+    website_link = db.Column(db.String(120))
+    seeking_talent = db.Column(db.Boolean,default=False)
+    seeking_description = db.Column(db.Text)
+    upcoming_shows_count = db.Column(db.Integer, default=0)
+    past_shows_count = db.Column(db.Integer, default=0)
+    shows = db.relationship('Show',backref='venue',lazy=True,cascade="save-update, merge, delete")
+
     def add(self):
         db.session.add(self)
         db.session.commit()
@@ -44,19 +48,21 @@ class Venue(db.Model):
 class Artist(db.Model):
     __tablename__ = 'Artist'
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    city = db.Column(db.String(120))
-    state = db.Column(db.String(120))
-    phone = db.Column(db.String(120))
-    genres = db.Column(db.String(120))
+    name = db.Column(db.String, nullable=False)
+    city = db.Column(db.String(120), nullable=False)
+    state = db.Column(db.String(120), nullable=False)
+    phone = db.Column(db.String(120), nullable=False)
+    genres = db.Column(db.String(120), nullable=False)
     image_link = db.Column(db.String(500))
     facebook_link = db.Column(db.String(120))
+
     # TODO: implement any missing fields, as a database migration using Flask-Migrate
-    address = db.Column(db.String(120))
-    website = db.Column(db.String(120))
-    upcoming_counter = db.Column(db.Integer, default=0)
-    seekingVenue = db.Column(db.Boolean)
-    seekingDescription = db.Column(db.String(500))
+    website_link = db.Column(db.String(120))
+    seeking_venue = db.Column(db.Boolean,default=False)
+    seeking_description = db.Column(db.Text)
+    upcoming_shows_count = db.Column(db.Integer, default=0)
+    past_shows_count = db.Column(db.Integer, default=0)
+    shows = db.relationship('Show',backref='artist',lazy=True,cascade="save-update, merge, delete")
     def add(self):
         db.session.add(self)
         db.session.commit()
@@ -74,9 +80,9 @@ class Show(db.Model):
     __tablename__ = 'Show'
 
     id = db.Column(db.Integer, primary_key=True)
-    artist_id = db.Column(db.Integer, db.ForeignKey('Artist.id'), nullable=False)
-    venue_id = db.Column(db.Integer, db.ForeignKey('Venue.id'), nullable=False)
-    start_time = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    start_time = db.Column(db.DateTime, nullable=False)
+    artist_id = db.Column(db.Integer,db.ForeignKey('Artist.id'),nullable=False)
+    venue_id = db.Column(db.Integer,db.ForeignKey('Venue.id'),nullable=False)
     upcoming = db.Column(db.Boolean, nullable=False, default=True)
 
     def add(self):
